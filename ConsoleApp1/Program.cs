e917b20841da01113125e15be45eafd6a0770cf5
SameeraNavalkar <samnavalkar.gitB@gmail.com>
2023-09-22 12:43:58 +0530

    Added Class UserA

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World from Master!");
+            Console.WriteLine("Hello World from userA!");
         }
     }
 }
