33f86678fc7ae55c8775dc0cb854204460678975
SameeraNavalkar <samnavalkar.gitB@gmail.com>
2023-09-23 14:22:55 +0530

    added rmTest to test rm command

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,6 +9,8 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World from Master!");
             Console.WriteLine("Hello World from userA!");
             Console.WriteLine("Hello World from user!");
+            Console.WriteLine("New line to check diff command");
+            Console.WriteLine("Another line to check diff command");
         }
     }
 }
